45414a8472f5790251b95c42b8362c95ffe377bf
mateusz-1992 <mateusz.marculewicz@gmail.com>
2024-02-28 20:48:05 +0100

    Finish Chater 1

---
+++ Client/Service/ProductService/ProductService.cs
@@ -0,0 +1,23 @@
+﻿using BlazorEcommerce.Shared;
+using System.Net.Http.Json;
+
+namespace BlazorEcommerce.Client.Service.ProductService
+{
+    public class ProductService : IProductService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ProductService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public List<Product> Products { get ; set ; } = new List<Product>();
+
+        public async Task GetProducts()
+        {
+            var result = await _httpClient.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/product");
+            if(result != null && result.Data != null) 
+            Products = result.Data;
+        }
+    }
+}
---
+++ Server/Service/ProductService/IProductService.cs
@@ -0,0 +1,7 @@
+﻿namespace BlazorEcommerce.Server.Service.ProductService
+{
+    public interface IProductService
+    {
+        Task<ServiceResponse<List<Product>>> GetProductsAsync();
+    }
+}
